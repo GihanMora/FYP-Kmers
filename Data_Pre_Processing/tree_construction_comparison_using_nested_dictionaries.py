17ba427cba1d2c5a6ed86621f8da7cd56da1989b
Gihan Gamage <gihangamage2015@gmail.com>
2019-03-29 07:40:12 +0530

    Cleaned the script and added comments

--- Data_Pre_Processing/tree_construction_comparison_using_nested_dictionaries.py
+++ Data_Pre_Processing/tree_construction_comparison_using_nested_dictionaries.py
@@ -1,29 +1,10 @@
 import datetime
 import os
-
 from Data_Pre_Processing.CSV_operations import convert_csv_column_to_list
 from Data_Pre_Processing.Kmer_indexing import Tree
 
-nested_dict1 = { 'A': {'C': {'C': {'C':{}}}},
-                'G': {'C': {'G': {'T':{}}}}}
-
-
-nested_dict2 = { 'A': {'C': {'C': {'C':{}}}},
-                'C': {'C': {'G': {'T':{},}}}}
-d1= { 'G': {'C': {'C': {},'A':{}
-                  }
-            },
-        'A': {'C': {'C': {},'T': {}
-                  }
-            }
-
-
-      }
-d2= { 'A': {'C': {'G': {}
-                  }
-            }
-      }
-
+# test1= { 'G': {'C': {'C': {},'A':{}}},'A': {'C': {'C': {},'T': {}}}}
+# test2= { 'A': {'C': {'G': {}}}}
 
 
 def add_kmer1(dict, kmer):
@@ -32,9 +13,7 @@ def add_kmer1(dict, kmer):
 
         ptr = dict[kmer[0]]
         itr = 1
-        #
         while itr < len(kmer):
-        #
             if kmer[itr] not in ptr.keys():
                 ptr[kmer[itr]] = {}
             ptr = ptr[kmer[itr]]
@@ -45,15 +24,11 @@ def add_kmer1(dict, kmer):
 
 def has_kmer(dict, kmer):
         itr = 0
-
         if not kmer[0] in dict.keys():
             return False
-
         ptr = dict[kmer[0]]
         itr = 1
-
         while itr < len(kmer):
-
             if kmer[itr] not in ptr.keys():
                 return False
             else:
@@ -85,39 +60,14 @@ class Summer:
 def nested_tree_comparison(dict1,dict2,summ):
 
     for k in dict1.keys():
-        # print(k,dict2.keys())
         if (k not in dict2.keys()):
-            # print(get_child_count(dict1[k]))
-            # print('dif',dict1[k])
             if(dict1[k]=={}):
-                # different_kmers+=1
-                # print(different_kmers)
-                # print(1)
                 summ.summing+=1
-                # print(summ.summing,'l')
-
             else:
-                # different_kmers+=get_child_count(dict1[k])
-                # print(get_child_count(dict1[k]))
                 summ.summing += get_child_count(dict1[k])
-                # print(summ.summing,'p')
-
         else:
-            # print("rec")
-            # print(dict1[k],dict2[k])
-            # print(summ.summing,'out')
+
             nested_tree_comparison(dict1[k], dict2[k],summ)
-            # for k1 in dict1[k].keys():
-            #     if (k1 not in dict2[k].keys()):
-            #         # print(get_child_count(dict1[k]))
-            #         different_kmers += get_child_count(dict1[k][k1])
-            #     else:
-            #         for k2 in dict1[k][k1].keys():
-            #             if (k2 not in dict2[k][k1].keys()):
-            #                 # print(get_child_count(dict1[k]))
-            #                 different_kmers += get_child_count(dict1[k][k1[k2]])
-    # return different_kmers
-    # print(sum,'last')
 
 csv_file_list_path = "/home/castle/FYP-KMER/KMERData_Results/KMERoutputs/13mer/CSV/test/"
 CSVFileList=os.listdir(csv_file_list_path)
@@ -136,31 +86,30 @@ for each_CSV_file in CSVFileList:
     time1 = datetime.datetime.now()
     for each_kmer in kmer_list:
         add_kmer1(dict,each_kmer)
-    time2 = datetime.datetime.now()
     print("done")
     time2 = datetime.datetime.now()
     print("tree construction finished : "+str(time2-time1))
     all_dicts.append(dict)
 
 
-
+    #comparison by iterating all kmers
     # count = 0
     # for each_kmer in kmer_list:
     #     if(has_kmer(dict, each_kmer)):
     #         count=count+1
     # print(count)
-# summ = Summer()
-# nested_tree_comparison(d1,d2,summ)
-# print(summ.summing)
-# #
+
 time22 = datetime.datetime.now()
 print(all_dicts[1].keys())
+
+#new tree comparison method
 summ = Summer()
 nested_tree_comparison(all_dicts[0], all_dicts[1],summ)
 time3 = datetime.datetime.now()
 print(summ.summing)
 print(time3 - time22)
 
+#tree comparison using set intersection method
 time4 = datetime.datetime.now()
 print(len(set(k_lists[0])-set(k_lists[1])))
 print(datetime.datetime.now() - time4)
